6e065ee238b090cb006a2356d9f2aa2d4c34c5d2
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2022-01-26 14:39:37 +0100

    push

--- generate_images.py
+++ generate_images.py
@@ -3,6 +3,7 @@
 import asyncio
 import os
 import re
+from time import strftime
 
 import aiohttp
 
@@ -35,7 +36,7 @@ async def generate_overview(s: Stats) -> None:
     with open("templates/overview.svg", "r") as f:
         output = f.read()
 
-    # output = re.sub("{{ name }}", await s.name, output)
+    output = re.sub("{{ time }}", await strftime("%d/%m - %H:%M"), output)
     output = re.sub("{{ stars }}", f"{await s.stargazers:,}", output)
     output = re.sub("{{ forks }}", f"{await s.forks:,}", output)
     output = re.sub("{{ contributions }}", f"{await s.total_contributions:,}", output)
