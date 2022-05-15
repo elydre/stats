382d1c9551807b3e656a9f2f74933279bda6971f
pf4-DEV <74263128+elydre@users.noreply.github.com>
2022-05-15 09:42:18 +0200

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -28,7 +28,7 @@ def generate_output_folder() -> None:
 ################################################################################
 
 async def get_number_of_projects() -> None:
-    md = await requests.get("https://raw.githubusercontent.com/elydre/elydre/main/all.md").text
+    md = requests.get("https://raw.githubusercontent.com/elydre/elydre/main/all.md").text
     return len(md.split("!")) - 1
 
 
